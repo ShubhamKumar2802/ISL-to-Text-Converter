349834e73b740ad2f0d5ef03d892e4dc112aae15
Aniket Kumar <56200361+30Aniket@users.noreply.github.com>
2020-12-17 03:56:47 +0530

    REVISED MODEL FOR ISL ALPHABETS AND NUMBERS WITH SELF-MADE DATASET.

--- Test/test.py
+++ Test/test.py
@@ -1,12 +1,13 @@
-from Data.data import get_train_test_split
-import keras
+from Data.load_data import get_train_test_split
 from sklearn.metrics import classification_report
 import keras
 
 train_data, test_data, train_target, test_target = get_train_test_split()
-model = keras.models.load_model("C:\\Users\\Aniket\\Desktop\\MINI PROJECT\\our_dataset_model")
+
+model = keras.models.load_model("C:\\Users\\Aniket\\Desktop\\MINI PROJECT\\our_dataset_new_model")
 predictions = model.predict(test_data)
 score = model.evaluate(test_data, test_target)
 print(score)
-report = classification_report(test_target, predictions.round())
+
+report = classification_report(test_target, predictions)
 print(report)
