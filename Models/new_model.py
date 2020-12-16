971241917774bf5127b97b4d81b18bee6dd43270
Aniket Kumar <56200361+30Aniket@users.noreply.github.com>
2020-12-17 03:50:11 +0530

    REVISED MODEL FOR ISL ALPHABETS AND NUMBERS WITH SELF-MADE DATASET.

---
+++ Models/new_model.py
@@ -0,0 +1,32 @@
+from Data.load_data import get_train_test_split
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPool2D, Flatten, Dropout, Dense
+from keras.optimizers import Adam
+from keras.callbacks import ReduceLROnPlateau, EarlyStopping
+
+train_data, test_data, train_target, test_target = get_train_test_split()
+
+model = Sequential()
+
+model.add(Conv2D(filters=32, kernel_size=(3, 3), activation='relu', input_shape=(100, 100, 1)))
+model.add(MaxPool2D(pool_size=(2, 2), strides=2))
+model.add(Conv2D(filters=64, kernel_size=(3, 3), activation='relu', padding='same'))
+model.add(MaxPool2D(pool_size=(2, 2), strides=2))
+model.add(Conv2D(filters=128, kernel_size=(3, 3), activation='relu', padding='valid'))
+model.add(MaxPool2D(pool_size=(2, 2), strides=2))
+
+model.add(Flatten())
+model.add(Dropout(0.5))
+
+model.add(Dense(64, activation="relu"))
+model.add(Dense(128, activation="relu"))
+model.add(Dense(128, activation="relu"))
+model.add(Dense(36, activation="softmax"))
+
+model.compile(optimizer=Adam(learning_rate=0.00001), loss='categorical_crossentropy', metrics=['accuracy'])
+reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=1, min_lr=0.0005)
+early_stop = EarlyStopping(monitor='val_loss', min_delta=0, patience=2, verbose=0, mode='auto')
+
+model.fit(train_data, train_target, epochs=15, callbacks=[reduce_lr, early_stop], validation_split=0.2)
+model.save("C:\\Users\\Aniket\\Desktop\\MINI PROJECT\\our_dataset_new_model")
+print(model.summary())
--- Models/old_model.py
+++ Models/old_model.py
@@ -1,4 +1,4 @@
-from Data.data import get_train_test_split
+from Data.load_data import get_train_test_split
 import keras
 import numpy as np
 from keras.models import Sequential
@@ -8,7 +8,7 @@ train_data, test_data, train_target, test_target = get_train_test_split()
 
 model = Sequential()
 
-model.add(Conv2D(100, (3, 3), activation='relu', input_shape=(100, 100, 1)))
+model.add(Conv2D(75, (3, 3), activation='relu', input_shape=(100, 100, 1)))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
 # model.add(Conv2D(100, (3, 3), activation='relu'))
@@ -23,10 +23,10 @@ model.add(Dropout(0.5))
 model.add(Dense(100, activation='relu'))
 # model.add(Dense(100, activation='relu'))
 # model.add(Dense(100, activation='relu'))
-# model.add(Dense(50, activation='relu'))
+model.add(Dense(50, activation='relu'))
 model.add(Dense(36, activation='softmax'))
 
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
 
 model.fit(train_data, train_target, epochs=3, validation_split=0.2)
 # model.save("/Volumes/Samsung_T5/Data_Sets/isl_model_cnn/isl")
